4954e0214cae55a565c7e781aaeead2209cca0af
unknown <tanquanga6k10@gmail.com>
2024-05-01 14:41:21 +0700

    update theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Button from '@mui/material/Button'
 import { AccessAlarm, ThreeDRotation } from '@mui/icons-material'
-
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <div>nguyentanquang</div>
+      <Typography variant='h1' color='text.secondary'> abc </Typography>
       <AccessAlarm color='success' />
-      <ThreeDRotation fontSize='large'/>
+      <ThreeDRotation fontSize='large' />
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
