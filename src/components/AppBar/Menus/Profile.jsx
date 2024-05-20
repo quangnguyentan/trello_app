c50b24639162ff8a58a9dfbc554f7f59b3c39039
unknown <tanquanga6k10@gmail.com>
2024-05-21 02:45:21 +0700

    header

---
+++ src/components/AppBar/Menus/Profile.jsx
@@ -0,0 +1,75 @@
+import Box from '@mui/material/Box'
+import React from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import Avatar from '@mui/material/Avatar'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'basic-button-profile' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profile"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profile'
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width : 28, height : 28, mr : 2 }} /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width : 28, height : 28, mr : 2 }} /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profile
--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -15,8 +15,9 @@ function ModeSelect() {
     const selectedMode = event.target.value
     setMode(selectedMode)
   }
+
   return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+    <FormControl sx={{ minWidth : 120 }} size="small" >
       <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
       <Select
         labelId="label-select-dark-light-mode"
@@ -26,9 +27,9 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <div style={{ display : 'flex', alignItems : 'center', gap : '8px' }}>
+          <Box sx={{ display : 'flex', alignItems : 'center', gap: 1 }}>
             <LightModeIcon/> Light
-          </div>
+          </Box>
         </MenuItem>
         <MenuItem value="dark">
           <Box sx={{ display : 'flex', alignItems : 'center', gap: 1 }}>
